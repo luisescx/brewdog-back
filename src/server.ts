ca1e2865538e2436ba3716adddf65c91192011ec
luisescx <luis.esculapio@Live.com>
2022-06-19 15:32:23 -0300

    feat: added cron to initialize data source in case of errors

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,58 @@
 import "express-async-errors";
+import cron from "cron";
 import { AppDataSource } from "./data-source";
 import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
 import { AppError } from "./errors/AppError";
 
+const app = express();
+
 AppDataSource.setOptions({ host: "database" });
 
-AppDataSource.initialize()
-  .then(async (options) => {
-    const app = express();
+async function init() {
+  await AppDataSource.initialize()
+    .then(async (options) => {
+      app.use(express.json());
 
-    app.use(express.json());
+      app.use(router);
 
-    app.use(router);
+      app.listen(3333, () => console.log("Server is running!"));
 
-    app.listen(3333, () => console.log("Server is running!"));
+      app.use(
+        (
+          err: Error,
+          request: Request,
+          response: Response,
+          nextFunction: NextFunction
+        ) => {
+          if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+              message: err.message,
+              statusCode: err.statusCode,
+            });
+          }
 
-    app.use(
-      (
-        err: Error,
-        request: Request,
-        response: Response,
-        nextFunction: NextFunction
-      ) => {
-        if (err instanceof AppError) {
-          return response.status(err.statusCode).json({
-            message: err.message,
-            statusCode: err.statusCode,
+          return response.status(500).json({
+            status: "error",
+            message: `Internal server error - ${err.message}`,
           });
         }
+      );
 
-        return response.status(500).json({
-          status: "error",
-          message: `Internal server error - ${err.message}`,
-        });
-      }
-    );
-  })
-  .catch((error) => console.log(error));
+      job.stop();
+    })
+    .catch((error) => console.log(error));
+}
+
+const job = new cron.CronJob(
+  "*/10 * * * * *",
+  async function () {
+    console.log("Connecting...");
+
+    await init();
+  },
+  null,
+  true
+);
+
+job.start();
